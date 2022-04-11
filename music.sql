86c9061f352e5cb7e72cc50aeaa761a3b42aeab1
jaino <wlsghtmeod@gmail.com>
2022-04-11 11:32:17 +0900

    Assignment

--- music.sql
+++ music.sql
@@ -1,1 +1,22 @@
+CREATE TABLE ARTISTS
+(
+    id INTEGER PRIMARY KEY
+    name TEXT,
+);
+
+CREATE TABLE ALBUMS
+(
+    id INTEGER PRIMARY KEY
+    name TEXT,
+    artists TEXT,
+);
+
+CREATE TABLE SONGS
+(
+    id INTEGER PRIMARY KEY
+    name TEXT,
+    album TEXT,
+    trackNum INTEGER,
+    time INTEGER
+);
 
